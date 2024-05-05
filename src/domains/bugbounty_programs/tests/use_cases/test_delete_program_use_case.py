7c101388a7ddae5e387f1714007e1a34cf32a7bc
Oleg Petrakov <petrakov.oleg@yandex.ru>
2024-05-05 13:55:23 +0400

    Add delete program use case and tests

---
+++ src/domains/bugbounty_programs/tests/use_cases/test_delete_program_use_case.py
@@ -0,0 +1,26 @@
+import pytest
+
+from asman.domains.bugbounty_programs.use_cases import DeleteProgramUseCase
+
+
+def test_delete_program_use_case_create():
+    delete_use_case = DeleteProgramUseCase(None, None)
+
+    assert delete_use_case
+    assert delete_use_case.repo
+
+
+@pytest.mark.asyncio
+async def test_read_program_use_case_execute(
+            create_program_use_case,
+            delete_program_use_case,
+            read_program_by_id_use_case,
+            program_data
+        ):
+    programId = await create_program_use_case.execute(program_data)
+    program = await read_program_by_id_use_case.execute(programId)
+    res = await delete_program_use_case.execute(programId)
+    deleted_program = await read_program_by_id_use_case.execute(programId)
+
+    assert res, 'The use case was executed successfully'
+    assert not deleted_program, 'The program was deleted'
---
+++ src/domains/bugbounty_programs/use_cases/delete_program_use_case.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+from asman.core.arch import AbstractUseCase
+from asman.core.adapters.db import Postgres, PostgresConfig
+
+from asman.domains.bugbounty_programs.api import ProgramId
+from asman.domains.bugbounty_programs.repo import ProgramRepository
+
+
+class DeleteProgramUseCase(AbstractUseCase):
+    def __init__(self, config: BaseSettings, databaseConfig: PostgresConfig, *argv) -> None:
+        database = Postgres(databaseConfig)
+        self.repo = ProgramRepository(database)
+
+    async def execute(self, programId: ProgramId) -> bool:
+        await self.repo.delete(programId)
+
+        return True
