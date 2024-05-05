5dc880c0fbba4dc2a2631f5ca3ece2f441915c56
Oleg Petrakov <petrakov.oleg@yandex.ru>
2024-05-05 18:15:50 +0400

    Update default values for config

--- src/core/adapters/db/postgres.py
+++ src/core/adapters/db/postgres.py
@@ -16,9 +16,9 @@ TableBase = declarative_base()
 
 
 class PostgresConfig(BaseSettings):
-    POSTGRES_DB: str = pydantic.Field()
-    POSTGRES_USER: str = pydantic.Field()
-    POSTGRES_PASSWORD: str = pydantic.Field()
+    POSTGRES_DB: str = pydantic.Field(default='my_db')
+    POSTGRES_USER: str = pydantic.Field(default='my_user')
+    POSTGRES_PASSWORD: str = pydantic.Field(default='my_password')
     POSTGRES_HOST: str = pydantic.Field(
         default='localhost'
     )
