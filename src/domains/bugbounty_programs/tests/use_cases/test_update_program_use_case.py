c3a569b589f513de21231266696328e46fccf8d4
Oleg Petrakov <petrakov.oleg@yandex.ru>
2024-05-05 13:54:57 +0400

    Add update program use case and tests

---
+++ src/domains/bugbounty_programs/tests/use_cases/test_update_program_use_case.py
@@ -0,0 +1,36 @@
+import copy
+import pytest
+
+from asman.domains.bugbounty_programs.api import Program
+from asman.domains.bugbounty_programs.use_cases import UpdateProgramUseCase
+
+
+def test_update_program_use_case_instance_create():
+    update_use_case = UpdateProgramUseCase(None, None)
+
+    assert update_use_case
+    assert update_use_case.repo
+
+
+@pytest.mark.asyncio
+async def test_update_program_use_case_execute(
+            create_program_use_case,
+            update_program_use_case,
+            program_data
+        ):
+
+    program_id = await create_program_use_case.execute(program_data)
+
+    copy_program_data = copy.deepcopy(program_data)
+    copy_program_data.program_name = 'SOME_NEW_VALUE'
+
+    updated_program = await update_program_use_case.execute(
+        Program(
+            id=program_id,
+            data=copy_program_data,
+        )
+    )
+
+    assert updated_program
+    assert updated_program.id.id == program_id.id
+    assert updated_program.data.program_name == 'SOME_NEW_VALUE'
---
+++ src/domains/bugbounty_programs/use_cases/update_program_use_case.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+from asman.core.arch import AbstractUseCase
+from asman.core.adapters.db import Postgres, PostgresConfig
+
+from asman.domains.bugbounty_programs.api import Asset, ProgramData, ProgramId, Program
+from asman.domains.bugbounty_programs.repo import ProgramRepository
+
+
+class UpdateProgramUseCase(AbstractUseCase):
+    def __init__(self, config: BaseSettings, databaseConfig: PostgresConfig, *argv) -> None:
+        database = Postgres(databaseConfig)
+        self.repo = ProgramRepository(database)
+
+    async def execute(self, program: Program) -> Program | None:
+        updated_program = await self.repo.update(program)
+
+        return updated_program
