95b01e22293cb1be25b3bf5be7d57d0a6478798e
Oleg Petrakov <petrakov.oleg@yandex.ru>
2024-12-06 11:56:43 +0400

    Update readme and remove getting certificates for each subscribe request

---
+++ src/domains/facebook_api/README.md
@@ -0,0 +1,5 @@
+# Проблемы
+
+- записывать как-то статусы: подписались на уведомления / отписались
+
+Проверить лимиты приложения: https://developers.facebook.com/apps/<APPLICATION-ID>/rate-limit-details/app/
--- src/domains/facebook_api/use_cases/subscribe_new_domains_use_case.py
+++ src/domains/facebook_api/use_cases/subscribe_new_domains_use_case.py
@@ -23,14 +23,17 @@ class SubscribeNewDomainsUseCase(AbstractUseCase):
 
         # 1. Get information from Facebook Graph API
         async with FacebookGraph('https://graph.facebook.com', self.config) as fb_client:
-            async def get_certificates(domain: str):
-                certs = await fb_client.get_certificates(domain)
-                cert_domains = list()
-                for cert in certs:
-                    cert_domains.extend(cert.domains)
-                result[domain] = cert_domains
+            # Из-за лимитов мы не можем так сделать: слишком много запросов на получение сертов с одного домена
 
-            await asyncio.gather(*[get_certificates(domain) for domain in domains])
+            # async def get_certificates(domain: str):
+            #     certs = await fb_client.get_certificates(domain)
+            #     cert_domains = list()
+            #     for cert in certs:
+            #         cert_domains.extend(cert.domains)
+            #     result[domain] = cert_domains
+
+            # await asyncio.gather(*[get_certificates(domain) for domain in domains])
+
         # 2. If it is new domain we'll subscribe for updates
             await fb_client.subscribe(domains)
 
