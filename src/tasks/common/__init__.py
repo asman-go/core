7b0c8c9fb949858caddda680cb97611fa3efa7ea
Oleg Petrakov <murami.ike@gmail.com>
2025-03-30 17:10:07 +0400

    add debug task

--- src/tasks/common/__init__.py
+++ src/tasks/common/__init__.py
@@ -1,6 +1,8 @@
+from .debug import DebugTask
 from .hello import hello as HelloTask
 
 
 __all__ = [
+    DebugTask,
     HelloTask,
 ]
---
+++ src/tasks/common/debug.py
@@ -0,0 +1,7 @@
+from celery import Task
+
+
+class DebugTask(Task):
+    def __call__(self, *args, **kwargs):
+        print('TASK STARTING: {0.name}[{0.request.id}]'.format(self))
+        return self.run(*args, **kwargs)
